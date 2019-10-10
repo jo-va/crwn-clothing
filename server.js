160e2dc7f83ef3abe825905572b4f6f515d02d5c
Jonathan Vandamme <jovndmm@gmail.com>
2019-10-10 14:46:45 -0700

    minor server and manifest changes

--- server.js
+++ server.js
@@ -14,14 +14,12 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compresion());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compresion());
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
